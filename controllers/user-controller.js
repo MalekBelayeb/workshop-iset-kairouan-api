9ae7bf5036ee8e208429f8a37c64bc0cbc233211
khalil-chermiti <khalil666chermiti@gmail.com>
2023-03-14 13:49:22 +0100

    feat(signin): add signin

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,25 +1,58 @@
-const req = require('express/lib/request')
-const User = require('../models/user-model')
+const req = require("express/lib/request");
+const userModel = require("../models/user-model");
+const User = require("../models/user-model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const register = async(req, res) => {
-    try {
-        console.log(req.body)
-        let { email, password, firstname, lastname } = req.body
-        const user = await User.findOne({ email })
-        if (user) {
-            return res.status(404).send({ success: false, message: "User already exist" })
-        }
-        const newUser = new User({ email, password, firstname, lastname })
-        const createUser = await newUser.save()
-        return res.status(200).send({ success: true, user: createUser })
-
-    } catch (err) {
-        console.log(err)
-        return res.status(404).send({ err })
-
+const register = async (req, res) => {
+  try {
+    console.log(req.body);
+    let { email, password, firstname, lastname } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(404)
+        .send({ success: false, message: "User already exist" });
     }
-}
+    const newUser = new User({ email, password, firstname, lastname });
+    const createUser = await newUser.save();
+    return res.status(200).send({ success: true, user: createUser });
+  } catch (err) {
+    console.log(err);
+    return res.status(404).send({ err });
+  }
+};
 
+const signin = async (req, res) => {
+  const { email, password } = req.body;
 
+  const user = await userModel
+    .findOne({ email })
+    .select("+isActive")
+    .select("+password");
 
-module.exports = { register }
+  if (!user)
+    return res.status(404).json({ message: "no user with such email !" });
+  console.log(user);
+  if (!user.isActive)
+    return res.status(404).json({ message: "account is banned ! " });
+
+  // compare password with hash
+  if (bcrypt.compareSync(password, user.password)) {
+    // sign jwt token
+    const token = jwt.sign(
+      { userID: user._id, role: user.role },
+      process.env.JWT_SECRET_TOKEN,
+      { expiresIn: "6h" }
+    );
+
+    delete user._doc.password;
+    delete user._doc.isActive;
+
+    return res.status(200).json({ token, user });
+  } else {
+    return res.status(404).json({ message: "wrong password !" });
+  }
+};
+
+module.exports = { register, signin };
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,6 @@ const route = require('express').Router()
 const userController = require('../controllers/user-controller')
 
 route.post('/v1/user/register', userController.register)
-
+route.post('/v1/user/login' , userController.signin ) ;
 
 module.exports = route
